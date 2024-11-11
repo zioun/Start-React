cd5d0c73c3845777b95ff31e3e76cf8bdf42a9b2
Zioun <jionkhan0@gmail.com>
2024-11-11 23:24:22 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from "react"; 
+import DaisyNav from "./components/DaisyNav";
 
 const App = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <DaisyNav></DaisyNav>
+      <h1 className={`text-3xl font-bold underline ${open ? '' : 'hidden'}`}>Hello world!</h1>
+      <button onClick={() => setOpen(!open)} className="px-10 border bg-red-500 text-white">Onclick</button>
     </div>
   );
 };
 
 export default App;
-
 
